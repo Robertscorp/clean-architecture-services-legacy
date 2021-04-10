ad8f49361a2d8de4036bcf5eda753f741ed5f5f8
James Roberts <James@Robertscom.com>
2021-04-10 23:49:52 +1000

    Add the GetGenders use case and tests
    CAS-15

---
+++ CleanArchitecture.Example.Application.Tests/Unit/UseCases/Person/GetGenders/GetGendersInteractorTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using CleanArchitecture.Example.Application.Services;
+using CleanArchitecture.Example.Application.UseCases.Person.GetGenders;
+using CleanArchitecture.Example.Domain.Enumerations;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Example.Application.Tests.Unit.UseCases.Person.GetGenders
+{
+
+    public class GetGendersInteractorTests
+    {
+
+        #region - - - - - - HandleAsync Tests - - - - - -
+
+        [Fact]
+        public async Task HandleAsync_AnyRequest_PresentsSuccessfully()
+        {
+            // Arrange
+            var _CancellationToken = new CancellationToken();
+            var _GenderDtos = new List<GenderDto>();
+
+            var _MockMapper = new Mock<IMapper>();
+            _MockMapper
+                .Setup(mock => mock.Map<List<GenderDto>>(It.IsAny<IEnumerable<GenderEnumeration>>()))
+                .Returns(_GenderDtos);
+
+            var _MockPresenter = new Mock<IPresenter<IQueryable<GenderDto>>>();
+
+            var _Interactor = new GetGendersInteractor(_MockMapper.Object);
+
+            // Act
+            await _Interactor.HandleAsync(new GetGendersRequest(), _MockPresenter.Object, _CancellationToken);
+
+            // Assert
+            _MockMapper.Verify(mock => mock.Map<List<GenderDto>>(It.IsAny<IEnumerable<GenderEnumeration>>()));
+            _MockPresenter.Verify(mock => mock.PresentAsync(It.IsAny<IQueryable<GenderDto>>(), _CancellationToken));
+
+            _MockMapper.VerifyNoOtherCalls();
+            _MockPresenter.VerifyNoOtherCalls();
+        }
+
+        #endregion HandleAsync Tests
+
+    }
+
+}
---
+++ CleanArchitecture.Example.Application/UseCases/Person/GetGenders/GetGendersProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CleanArchitecture.Example.Domain.Enumerations;
+
+namespace CleanArchitecture.Example.Application.UseCases.Person.GetGenders
+{
+    public class GetGendersProfile : Profile
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GetGendersProfile()
+        {
+            _ = this.CreateMap<GenderEnumeration, GenderDto>()
+                    .ForMember(dest => dest.GenderID, opts => opts.MapFrom(src => src.ToEntityID()))
+                    .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.ToString()));
+        }
+
+        #endregion Constructors
+
+    }
+}
