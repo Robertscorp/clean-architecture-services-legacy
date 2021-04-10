eebfb42df288484a46499f369af8a16b11c080d1
James Roberts <James@Robertscom.com>
2021-04-10 15:53:48 +1000

    Add Entities
    CAS-0

---
+++ CleanArchitecture.Services/Entities/EntityRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchitecture.Services.Entities
+{
+
+    public class EntityRequest
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public string EntityName { get; set; }
+
+        public IEnumerable<EntityKeyProperty> Keys { get; set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        public static EntityRequest GetEntityRequest(string propertyName, object value)
+            => GetEntityRequest(null, propertyName, value);
+
+        public static EntityRequest GetEntityRequest(string entityName, string propertyName, object value)
+            => new EntityRequest() { EntityName = entityName, Keys = new[] { EntityKeyProperty.GetEntityKeyProperty(propertyName, value) } };
+
+        #endregion Methods
+
+    }
+
+}
