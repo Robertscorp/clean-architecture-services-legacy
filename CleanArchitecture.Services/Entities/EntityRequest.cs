6dca3419b6785f246ead812504f18f60469df60c
James Roberts <James@Robertscom.com>
2021-04-10 15:59:26 +1000

    Rename EntityName to EntityType
    CAS-0

--- CleanArchitecture.Services/Entities/EntityRequest.cs
+++ CleanArchitecture.Services/Entities/EntityRequest.cs
@@ -8,7 +8,7 @@ namespace CleanArchitecture.Services.Entities
 
         #region - - - - - - Properties - - - - - -
 
-        public string EntityName { get; set; }
+        public string EntityType { get; set; }
 
         public IEnumerable<EntityKeyProperty> Keys { get; set; }
 
@@ -19,8 +19,8 @@ namespace CleanArchitecture.Services.Entities
         public static EntityRequest GetEntityRequest(string propertyName, object value)
             => GetEntityRequest(null, propertyName, value);
 
-        public static EntityRequest GetEntityRequest(string entityName, string propertyName, object value)
-            => new EntityRequest() { EntityName = entityName, Keys = new[] { EntityKeyProperty.GetEntityKeyProperty(propertyName, value) } };
+        public static EntityRequest GetEntityRequest(string entityType, string propertyName, object value)
+            => new EntityRequest() { EntityType = entityType, Keys = new[] { EntityKeyProperty.GetEntityKeyProperty(propertyName, value) } };
 
         #endregion Methods
 
