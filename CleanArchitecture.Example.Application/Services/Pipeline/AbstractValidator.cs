9bab2f22fdfff3a433c8a64a91f1b74d31385bf4
James Roberts <James@Robertscom.com>
2021-04-12 22:38:35 +1000

    Add the Abstract Validator
    CAS-15

---
+++ CleanArchitecture.Example.Application/Services/Pipeline/AbstractValidator.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Services.Pipeline;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Example.Application.Services.Pipeline
+{
+
+    public abstract class AbstractValidator<TRequest> : FluentValidation.AbstractValidator<TRequest>, IRequestValidator<TRequest, ValidationResult>
+    {
+
+        #region - - - - - - IRequestValidator Implementation - - - - - -
+
+        async Task<ValidationResult> IRequestValidator<TRequest, ValidationResult>.ValidateAsync(TRequest request, CancellationToken cancellationToken)
+            => new ValidationResult(await base.ValidateAsync(request, cancellationToken));
+
+        #endregion IRequestValidator Implementation
+
+    }
+
+}
--- CleanArchitecture.Example.Application/Services/Pipeline/ValidationResult.cs
+++ CleanArchitecture.Example.Application/Services/Pipeline/ValidationResult.cs
@@ -1,10 +1,51 @@
 ﻿using CleanArchitecture.Services.Entities;
+using System;
+using System.Linq.Expressions;
 
 namespace CleanArchitecture.Example.Application.Services.Pipeline
 {
 
     public class ValidationResult : FluentValidation.Results.ValidationResult, IValidationResult
     {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ValidationResult(FluentValidation.Results.ValidationResult validationResult) : base(validationResult.Errors)
+            => this.RuleSetsExecuted = validationResult.RuleSetsExecuted;
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public new string[] RuleSetsExecuted
+        {
+            get => base.RuleSetsExecuted;
+            set => SetRuleSetsExecuted(this, value);
+        }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        private static void SetRuleSetsExecuted(ValidationResult validationResult, string[] value)
+        {
+            if (s_SetRuleSetsExecutedAction == null)
+            {
+                var _ValidationResultParameterExpression = Expression.Parameter(typeof(FluentValidation.Results.ValidationResult));
+                var _ValueParameterExpression = Expression.Parameter(typeof(string[]));
+                var _PropertyExpression = Expression.Property(_ValidationResultParameterExpression, nameof(RuleSetsExecuted));
+                var _PropertyAssignExpression = Expression.Assign(_PropertyExpression, _ValueParameterExpression);
+                var _LambdaExpression = Expression.Lambda<Action<FluentValidation.Results.ValidationResult, string[]>>(_PropertyAssignExpression, _ValidationResultParameterExpression, _ValueParameterExpression);
+
+                s_SetRuleSetsExecutedAction = _LambdaExpression.Compile();
+            }
+
+            s_SetRuleSetsExecutedAction.Invoke(validationResult, value);
+        }
+        private static Action<FluentValidation.Results.ValidationResult, string[]> s_SetRuleSetsExecutedAction;
+
+        #endregion Methods
+
     }
 
 }
