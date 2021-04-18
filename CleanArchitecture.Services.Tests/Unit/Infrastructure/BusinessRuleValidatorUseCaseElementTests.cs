17512c69293501c5e3c9f9e092bbe5b1ca4c486e
James Roberts <James@Robertscom.com>
2021-04-18 14:53:15 +1000

    Add BusinessRuleValidatorUseCaseElement infrastructure and tests
    CAS-25

---
+++ CleanArchitecture.Services.Tests/Unit/Infrastructure/BusinessRuleValidatorUseCaseElementTests.cs
@@ -0,0 +1,103 @@
+﻿using CleanArchitecture.Services.Entities;
+using CleanArchitecture.Services.Infrastructure;
+using CleanArchitecture.Services.Pipeline;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Services.Tests.Unit.Infrastructure
+{
+
+    public class BusinessRuleValidatorUseCaseElementTests
+    {
+
+        #region - - - - - - TryPresentResultAsync Tests - - - - - -
+
+        [Fact]
+        public async Task TryPresentResultAsync_RequestFailsBusinessRuleValidation_PresentsValidationFailureAndReturnsTrue()
+        {
+            // Arrange
+            var _CancellationToken = new CancellationToken();
+            var _Request = new Mock<IUseCaseRequest<object>>().Object;
+
+            var _MockValidationResult = new Mock<IValidationResult>();
+            _ = _MockValidationResult
+                    .Setup(mock => mock.IsValid)
+                    .Returns(false);
+
+            var _MockBusinessRuleValidator = new Mock<IBusinessRuleValidator<IUseCaseRequest<object>, IValidationResult>>();
+            _ = _MockBusinessRuleValidator
+                    .Setup(mock => mock.ValidateAsync(_Request, _CancellationToken))
+                    .Returns(Task.FromResult(_MockValidationResult.Object));
+
+            var _MockServiceProvider = new Mock<IServiceProvider>();
+            _ = _MockServiceProvider
+                    .Setup(mock => mock.GetService(typeof(IBusinessRuleValidator<IUseCaseRequest<object>, IValidationResult>)))
+                    .Returns(_MockBusinessRuleValidator.Object);
+
+            var _MockPresenter = new Mock<IPresenter<object, IValidationResult>>();
+
+            var _Element = new BusinessRuleValidatorUseCaseElement<object, IValidationResult>(_MockServiceProvider.Object);
+
+            // Act
+            var _Actual = await _Element.TryPresentResultAsync(_Request, _MockPresenter.Object, _CancellationToken);
+
+            // Assert
+            _ = _Actual.Should().BeTrue();
+
+            _MockBusinessRuleValidator.Verify(mock => mock.ValidateAsync(_Request, _CancellationToken), Times.Once);
+            _MockPresenter.Verify(mock => mock.PresentValidationFailureAsync(_MockValidationResult.Object, _CancellationToken), Times.Once);
+            _MockValidationResult.Verify(mock => mock.IsValid);
+
+            _MockBusinessRuleValidator.VerifyNoOtherCalls();
+            _MockPresenter.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task TryPresentResultAsync_RequestPassesBusinessRuleValidation_DoesntPresentAnythingAndReturnsFalse()
+        {
+            // Arrange
+            var _CancellationToken = new CancellationToken();
+            var _Request = new Mock<IUseCaseRequest<object>>().Object;
+
+            var _MockValidationResult = new Mock<IValidationResult>();
+            _ = _MockValidationResult
+                    .Setup(mock => mock.IsValid)
+                    .Returns(true);
+
+            var _MockBusinessRuleValidator = new Mock<IBusinessRuleValidator<IUseCaseRequest<object>, IValidationResult>>();
+            _ = _MockBusinessRuleValidator
+                    .Setup(mock => mock.ValidateAsync(_Request, _CancellationToken))
+                    .Returns(Task.FromResult(_MockValidationResult.Object));
+
+            var _MockServiceProvider = new Mock<IServiceProvider>();
+            _ = _MockServiceProvider
+                    .Setup(mock => mock.GetService(typeof(IBusinessRuleValidator<IUseCaseRequest<object>, IValidationResult>)))
+                    .Returns(_MockBusinessRuleValidator.Object);
+
+            var _MockPresenter = new Mock<IPresenter<object, IValidationResult>>();
+
+            var _Element = new BusinessRuleValidatorUseCaseElement<object, IValidationResult>(_MockServiceProvider.Object);
+
+            // Act
+            var _Actual = await _Element.TryPresentResultAsync(_Request, _MockPresenter.Object, _CancellationToken);
+
+            // Assert
+            _ = _Actual.Should().BeFalse();
+
+            _MockBusinessRuleValidator.Verify(mock => mock.ValidateAsync(_Request, _CancellationToken), Times.Once);
+            _MockPresenter.Verify(mock => mock.PresentValidationFailureAsync(_MockValidationResult.Object, _CancellationToken), Times.Never);
+            _MockValidationResult.Verify(mock => mock.IsValid);
+
+            _MockBusinessRuleValidator.VerifyNoOtherCalls();
+            _MockPresenter.VerifyNoOtherCalls();
+        }
+
+        #endregion TryPresentResultAsync Tests
+
+    }
+
+}
---
+++ CleanArchitecture.Services/Infrastructure/BusinessRuleValidatorUseCaseElement.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Services.Entities;
+using CleanArchitecture.Services.Pipeline;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Services.Infrastructure
+{
+
+    public class BusinessRuleValidatorUseCaseElement<TResponse, TValidationResult> : IUseCaseElement<TResponse, TValidationResult> where TValidationResult : IValidationResult
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly IServiceProvider m_ServiceProvider;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public BusinessRuleValidatorUseCaseElement(IServiceProvider serviceProvider)
+            => this.m_ServiceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+
+        #endregion Constructors088.54
+
+        #region - - - - - - IUseCaseElement Implementation - - - - - -
+
+        public async Task<bool> TryPresentResultAsync<TPresenter, TRequest>(TRequest request, TPresenter presenter, CancellationToken cancellationToken)
+            where TPresenter : IPresenter<TResponse, TValidationResult>
+            where TRequest : IUseCaseRequest<TResponse>
+        {
+            var _BusinessRuleValidator = (IBusinessRuleValidator<TRequest, TValidationResult>)this.m_ServiceProvider.GetService(typeof(IBusinessRuleValidator<TRequest, TValidationResult>));
+            if (_BusinessRuleValidator != null)
+            {
+                var _ValidationResult = await _BusinessRuleValidator.ValidateAsync(request, cancellationToken);
+                if (!_ValidationResult.IsValid)
+                {
+                    await presenter.PresentValidationFailureAsync(_ValidationResult, cancellationToken);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion IUseCaseElement Implementation
+
+    }
+
+}
