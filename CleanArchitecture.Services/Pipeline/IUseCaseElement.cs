8ea4f3ea4bdb5338a5e930e74ef28f3c4263a8c8
James Roberts <James@Robertscom.com>
2021-04-18 12:00:31 +1000

    Add the UseCaseElement service
    CAS-25

---
+++ CleanArchitecture.Services/Pipeline/IUseCaseElement.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Services.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Services.Pipeline
+{
+
+    public interface IUseCaseElement<TResponse, TValidationResult> where TValidationResult : IValidationResult
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        Task<bool> TryPresentResultAsync<TPresenter, TRequest>(TRequest request, TPresenter presenter, CancellationToken cancellationToken)
+            where TPresenter : IPresenter<TResponse, TValidationResult>
+            where TRequest : IUseCaseRequest<TResponse>;
+
+        #endregion Methods
+
+    }
+
+}
