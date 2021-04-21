4e0797d20dc8504256722027c8e8edbdee196e4d
James Roberts <James@Robertscom.com>
2021-04-21 23:02:57 +1000

    Add a constructor
    CAS-34

--- CleanArchitecture.Example.InterfaceAdapters/ViewModels/Genders/ExistingGenderViewModel.cs
+++ CleanArchitecture.Example.InterfaceAdapters/ViewModels/Genders/ExistingGenderViewModel.cs
@@ -6,6 +6,16 @@ namespace CleanArchitecture.Example.InterfaceAdapters.ViewModels.Genders
     public class ExistingGenderViewModel
     {
 
+        #region - - - - - - Constructors - - - - - -
+
+        public ExistingGenderViewModel(EntityID genderID, string name)
+        {
+            this.GenderID = genderID;
+            this.Name = name;
+        }
+
+        #endregion Constructors
+
         #region - - - - - - Properties - - - - - -
 
         public EntityID GenderID { get; private set; }
