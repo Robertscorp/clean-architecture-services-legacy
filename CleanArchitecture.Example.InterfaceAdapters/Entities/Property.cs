c9478bfefc70a9a933932e3a2e12449d338a0611
James Roberts <James@Robertscom.com>
2021-04-21 22:40:58 +1000

    Add the Property class and tests
    CAS-34

---
+++ CleanArchitecture.Example.InterfaceAdapters/Entities/Property.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CleanArchitecture.Example.InterfaceAdapters.Entities
+{
+
+    public class Property<TProperty>
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private TProperty m_Value;
+
+        #endregion Fields
+
+        #region - - - - - - Properties - - - - - -
+
+        public TProperty Value
+        {
+            get => this.m_Value;
+            set
+            {
+                if (!object.Equals(this.m_Value, value))
+                {
+                    this.m_Value = value;
+                    this.ValueChanged?.Invoke(value);
+                }
+            }
+        }
+
+        public Action<TProperty> ValueChanged { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
