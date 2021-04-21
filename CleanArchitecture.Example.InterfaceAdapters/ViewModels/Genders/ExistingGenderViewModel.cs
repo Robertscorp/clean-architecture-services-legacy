690628b99bb6580c84d7f4f2716622cd1f345ff8
James Roberts <James@Robertscom.com>
2021-04-21 22:41:50 +1000

    Add the ExistingGender ViewModel
    CAS-34

---
+++ CleanArchitecture.Example.InterfaceAdapters/ViewModels/Genders/ExistingGenderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Services.Entities;
+
+namespace CleanArchitecture.Example.InterfaceAdapters.ViewModels.Genders
+{
+
+    public class ExistingGenderViewModel
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public EntityID GenderID { get; private set; }
+
+        public string Name { get; private set; }
+
+        #endregion Properties
+
+    }
+
+}
