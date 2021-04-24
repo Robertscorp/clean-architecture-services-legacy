36fd1ad2b16cebfe1d562616f22574023c227a98
James Roberts <James@Robertscom.com>
2021-04-24 21:15:18 +1000

    Add the GenderController and tests
    CAS-36

---
+++ CleanArchitecture.Example.InterfaceAdapters.Tests/Unit/Controllers/GenderControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchitecture.Example.Application.Services.Pipeline;
+using CleanArchitecture.Example.Application.UseCases.People.GetGenders;
+using CleanArchitecture.Example.InterfaceAdapters.Controllers;
+using CleanArchitecture.Services.Pipeline;
+using Moq;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Example.InterfaceAdapters.Tests.Unit.Controllers
+{
+
+    public class GenderControllerTests
+    {
+
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly CancellationToken m_CancellationToken = new CancellationToken();
+        private readonly GenderController m_Controller;
+
+        private readonly Mock<IUseCaseInvoker> m_MockUseCaseInvoker = new Mock<IUseCaseInvoker>();
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GenderControllerTests()
+            => this.m_Controller = new GenderController(this.m_MockUseCaseInvoker.Object);
+
+        #endregion Constructors
+
+        #region - - - - - - GetGendersAsync Tests - - - - - -
+
+        [Fact]
+        public async Task GetGendersAsync_AnyRequest_InvokesUseCaseWithPresenterAndCancellationToken()
+        {
+            // Arrange
+            var _Presenter = new Mock<IPresenter<IQueryable<GenderDto>>>().Object;
+
+            // Act
+            await this.m_Controller.GetGendersAsync(_Presenter, this.m_CancellationToken);
+
+            // Assert
+            this.m_MockUseCaseInvoker.Verify(mock => mock.InvokeUseCaseAsync(It.IsAny<GetGendersRequest>(), _Presenter, this.m_CancellationToken));
+        }
+
+        #endregion GetGendersAsync Tests
+
+    }
+
+}
---
+++ CleanArchitecture.Example.InterfaceAdapters/Controllers/GenderController.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitecture.Example.Application.Services.Pipeline;
+using CleanArchitecture.Example.Application.UseCases.People.GetGenders;
+using CleanArchitecture.Services.Pipeline;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Example.InterfaceAdapters.Controllers
+{
+
+    public class GenderController
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly IUseCaseInvoker m_UseCaseInvoker;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GenderController(IUseCaseInvoker useCaseInvoker)
+            => this.m_UseCaseInvoker = useCaseInvoker ?? throw new System.ArgumentNullException(nameof(useCaseInvoker));
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public Task GetGendersAsync(IPresenter<IQueryable<GenderDto>> presenter, CancellationToken cancellationToken)
+            => this.m_UseCaseInvoker.InvokeUseCaseAsync(new GetGendersRequest(), presenter, cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
