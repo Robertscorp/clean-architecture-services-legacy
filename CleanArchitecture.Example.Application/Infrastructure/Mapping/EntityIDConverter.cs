88c78843014d97aaecf9fd0dbcc5d876ae994775
James Roberts <James@Robertscom.com>
2021-05-03 22:55:36 +1000

    Add the EntityIDConverter
    CAS-34

---
+++ CleanArchitecture.Example.Application/Infrastructure/Mapping/EntityIDConverter.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using CleanArchitecture.Services.Entities;
+using CleanArchitecture.Services.Persistence;
+using System;
+using System.Threading;
+
+namespace CleanArchitecture.Example.Application.Infrastructure.Mapping
+{
+
+    public class EntityIDConverter<TDestinationMember> : IValueConverter<EntityID, TDestinationMember> where TDestinationMember : class
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly IPersistenceContext m_PersistenceContext;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public EntityIDConverter(IPersistenceContext persistenceContext)
+            => this.m_PersistenceContext = persistenceContext ?? throw new ArgumentNullException(nameof(persistenceContext));
+
+        #endregion Constructors
+
+        #region - - - - - - IMemberValueResolver Implementation - - - - - -
+
+        public TDestinationMember Convert(EntityID sourceMember, ResolutionContext context)
+            => this.m_PersistenceContext.FindAsync<TDestinationMember>(sourceMember, CancellationToken.None).GetAwaiter().GetResult();
+
+        #endregion IMemberValueResolver Implementation
+
+    }
+
+}
