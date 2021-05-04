4da21d8b498a473892d48b2340af444a01b3d95f
James Roberts <James@Robertscom.com>
2021-05-04 19:56:25 +1000

    Make the Constructor single line
    CAS-34

--- CleanArchitecture.Example.Application/UseCases/People/GetGenders/GetGendersProfile.cs
+++ CleanArchitecture.Example.Application/UseCases/People/GetGenders/GetGendersProfile.cs
@@ -10,11 +10,9 @@ namespace CleanArchitecture.Example.Application.UseCases.People.GetGenders
         #region - - - - - - Constructors - - - - - -
 
         public GetGendersProfile()
-        {
-            _ = this.CreateMap<Gender, GenderDto>()
+            => _ = this.CreateMap<Gender, GenderDto>()
                     .ForMember(dest => dest.GenderID, opts => opts.MapFrom(src => src.ID))
                     .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.ToString()));
-        }
 
         #endregion Constructors
 
