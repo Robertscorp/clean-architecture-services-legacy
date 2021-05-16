12abce63bb82022bac2614b48c6abf18b96def56
James Roberts <James@Robertscom.com>
2021-05-16 19:44:24 +1000

    Add the UpdateEntity presenter
    CAS-34

---
+++ CleanArchitecture.Services.AspNetCore/Presenters/UpdateEntityPresenter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Services.AspNetCore.Presenters
+{
+
+    public class UpdateEntityPresenter<TResponse> : ActionPresenter<TResponse>
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public override Task PresentAsync(TResponse response, CancellationToken cancellationToken)
+        {
+            this.ActionResult = new OkObjectResult(response);
+            this.PresentedSuccessfully = true;
+            return Task.CompletedTask;
+        }
+
+        #endregion Methods
+
+    }
+
+}
