e95fc4b041a31cbbe0fd578ec4b4f0231e084b88
James Roberts <James@Robertscom.com>
2021-05-16 14:25:48 +1000

    Add BaseController to AspNetCore.Controllers
    CAS-48

---
+++ CleanArchitecture.Services.AspNetCore/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using CleanArchitecture.Services.AspNetCore.Presenters;
+using CleanArchitecture.Services.Extended.Pipeline;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Services.AspNetCore.Controllers
+{
+
+    public abstract class BaseController : ControllerBase
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public IMapper Mapper => (IMapper)this.HttpContext.RequestServices.GetService(typeof(IMapper));
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        public async Task<IActionResult> GetManyAsync<TResponse>(Func<IPresenter<IQueryable<TResponse>>, CancellationToken, Task> controllerAction)
+        {
+            var _QueryPresenter = new GetManyEntitiesPresenter<TResponse>() { Mapper = this.Mapper };
+
+            await controllerAction(_QueryPresenter, CancellationToken.None);
+
+            return _QueryPresenter.ActionResult;
+        }
+
+        #endregion Methods
+
+    }
+
+}
