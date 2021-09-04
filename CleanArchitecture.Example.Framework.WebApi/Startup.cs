39858e01d5531dca415475042b456e84ce276408
James Roberts <James@Robertscom.com>
2021-09-04 22:22:58 +1000

    Committing WIP

--- CleanArchitecture.Example.Framework.WebApi/Startup.cs
+++ CleanArchitecture.Example.Framework.WebApi/Startup.cs
@@ -1,9 +1,11 @@
 using CleanArchitecture.Example.Application.Dtos;
+using CleanArchitecture.Example.Domain.Entities;
 using CleanArchitecture.Example.InterfaceAdapters.Controllers;
 using CleanArchitecture.Services.Entities;
 using CleanArchitecture.Services.Extended.EFCore;
 using CleanArchitecture.Services.Extended.FluentValidation;
 using CleanArchitecture.Services.Extended.Pipeline;
+using CleanArchitecture.Services.Extended.Queryable;
 using CleanArchitecture.Services.Infrastructure;
 using CleanArchitecture.Services.Persistence;
 using CleanArchitecture.Services.Pipeline;
@@ -59,7 +61,8 @@ namespace CleanArchitecture.Example.Framework.WebApi
                                             .Where(type => type.GetInterface(typeof(IUseCaseElement<,>).Name) == null)
                                             .Where(type => type != typeof(PersistenceContext))
                                             .Where(type => type != typeof(QueryableUnion<>))
-                                            .Where(type => type != typeof(ValidationResult)))
+                                            .Where(type => type != typeof(ValidationResult))
+                                            .Where(type => !type.IsAssignableTo(typeof(IQueryable))))
                                     .AsImplementedInterfaces()
                                     .WithScopedLifetime());
 
@@ -69,6 +72,109 @@ namespace CleanArchitecture.Example.Framework.WebApi
                                     .WithScopedLifetime());
 
             _ = services.AddScoped<IPersistenceContext, TempPersistenceContext>();
+
+
+            var _PersonQuery1 = new[]
+            {
+                new Person { EmailAddress = "I" },
+                new Person { EmailAddress = "C" },
+                new Person { EmailAddress = "A" },
+                new Person { EmailAddress = "G" },
+                new Person { EmailAddress = "Y" }
+            }.AsQueryable();
+            var _PersonQuery2 = new[]
+            {
+                new Person { EmailAddress = "D" },
+                new Person { EmailAddress = "B" },
+                new Person { EmailAddress = "Z" },
+                new Person { EmailAddress = "X" },
+                new Person { EmailAddress = "E" }
+            }.AsQueryable();
+
+            //     var _A = new QueryableUnion<Person>(_PersonQuery1, _PersonQuery2);
+
+            //     var _X = _A.ProjectTo<CustomerDto>(new AutoMapper.MapperConfiguration(opts => opts.CreateMap<Person, CustomerDto>().ForMember(dest => dest.EmailAddress, opts => opts.MapFrom(src => src.EmailAddress)).ForAllOtherMembers(opts => opts.Ignore())));
+            //     var y = _X.ToList();
+
+            //var _PersonQuery1 = new[]
+            //{
+            //    new Person { EmailAddress = "J" },
+            //    new Person { EmailAddress = "K" },
+            //    new Person { EmailAddress = "L" },
+            //    new Person { EmailAddress = "M" },
+            //    new Person { EmailAddress = "N" }
+            //}.AsQueryable();
+
+            //var _PersonQuery2 = new Person[] { }.AsQueryable();
+            //var _PersonQuery3 = new Person[] { }.AsQueryable();
+            //var _PersonQuery4 = new Person[] { }.AsQueryable();
+
+            //var _PersonQuery5 = new[]
+            //{
+            //    new Person { EmailAddress = "A", FirstName = "B" },
+            //    new Person { EmailAddress = "A", FirstName = "A" },
+            //    new Person { EmailAddress = "C" },
+            //    new Person { EmailAddress = "E" }
+            //}.AsQueryable();
+
+            //var _PersonQuery6 = new[]
+            //{
+            //    new Person { EmailAddress = "B" },
+            //    new Person { EmailAddress = "D" }
+            //}.AsQueryable();
+
+            //var _PersonQuery7 = new[]
+            //{
+            //    new Person { EmailAddress = "3" },
+            //    new Person { EmailAddress = "2" },
+            //    new Person { EmailAddress = "1" }
+            //}.AsQueryable();
+
+            //var _QueryableUnion = new QueryableUnion<Person>(_PersonQuery1, _PersonQuery2, _PersonQuery3, _PersonQuery4, _PersonQuery5, _PersonQuery6, _PersonQuery7);
+            ////var _X = _QueryableUnion.Any();
+
+            ////var _X = _QueryableUnion.OrderBy(p => p.EmailAddress).ThenBy(p => p.FirstName).Take(1).Select(p => new { p.EmailAddress }).ToList();
+            ////var _X = _QueryableUnion.OrderBy(p => p.EmailAddress).Select(p => p.EmailAddress).OrderByDescending(x => x).ToList();
+            ////var _A = _QueryableUnion.OrderBy(p => new X(p)/*.EmailAddress, "A")*/).ToList();
+            //var _A = _QueryableUnion.OrderBy(p => p.EmailAddress);
+            //var _B = _A.Select(p => new { p.EmailAddress, p.FirstName, p.LastName });
+            ////var _C = _A.Where(x => x.EmailAddress == "A");
+
+            //xxx //This is currently working. It might make sense to change the internal implementation to be all expression based, instead of wrapping queryables.
+            //    // Before any refactoring, clean up the code and commit it. It's a slight improvement. maybe make it not re-apply the expression tree if a projection occurs (check to see if the types are assignable)
+            //    // All expression based means that instead of storing queryables, the queryables can be encapsulated into an expression tree.
+            //    // This would require a "public" expression, and a "private" expression. Actually, maybe the private expression isn't required.
+            //    // The expression tree is what's compiled and invoked. What that expression actually *means* is totally irrelevant for the consumer.
+
+            //var _Z = _A.ToList();
+            //var aa = 0;
+
+            //var _A = new XXX();
+            //var _B = new XYZ();
+            //var _C = _A.Concat(_B);
+
+            //var _A = _PersonQuery1.Concat(_PersonQuery2);
+
+            var x = new ConcatenatedQueryable<Person>(_PersonQuery1, _PersonQuery2);
+            var y = x.Select(p => p.EmailAddress);
+            var z = y.OrderBy(e => e);
+
+            var a = new ConcatenatedQueryable<string>(z, new[] { "T", "S", "R" }.AsQueryable());
+            var b = a.OrderBy(s => s);
+
+            //var xx = new ConcatenatedQueryableExpressionVisitor();
+            //var yy = xx.Visit(b.Expression);
+
+            //var xxx = b.GetEnumerator();
+
+
+            //xxx // This looks like it's working, but it's probably not. The reason is the first concatenation is being sorted together (by the orderby s => s).
+            //    // This means that the individual queries are being executed, then the second sort is being applied. If those were hits to the DB (and projections),
+            //    // the DB would be loading more than necessary. This needs to be investigated - the current approach is likely wrong.
+            //    // When a DuplicateTree encounters a DuplicateTree internally, it needs to apply it's expression to that DuplicateTree.
+
+            //var xyz = 0;
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -87,6 +193,51 @@ namespace CleanArchitecture.Example.Framework.WebApi
         }
 
         #endregion Methods
+
+    }
+
+
+    //public class XXX : IQueryable<string>
+    //{
+    //    public Type ElementType => throw new NotImplementedException();
+
+    //    public System.Linq.Expressions.Expression Expression => throw new NotImplementedException();
+
+    //    public IQueryProvider Provider => throw new NotImplementedException();
+
+    //    public IEnumerator<string> GetEnumerator() => throw new NotImplementedException();
+    //    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    //}
+
+    //public class XYZ : IQueryable<string>
+    //{
+    //    public Type ElementType => throw new NotImplementedException();
+
+    //    public System.Linq.Expressions.Expression Expression => throw new NotImplementedException();
+
+    //    public IQueryProvider Provider => throw new NotImplementedException();
+
+    //    public IEnumerator<string> GetEnumerator() => throw new NotImplementedException();
+    //    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    //}
+
+
+    public class X : IComparable
+    {
+        private readonly string m_A;
+        private readonly string m_B;
+
+        public X(Person p)
+        {
+            //this.m_A = a;
+            //this.m_B = b;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var _X = this.m_A.CompareTo(((X)obj).m_A);
+            return _X != 0 ? _X : this.m_B.CompareTo(((X)obj).m_B);
+        }
 
     }
 
---
+++ CleanArchitecture.Services.Extended/Queryable/ConcatenatedQueryable.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CleanArchitecture.Services.Extended.Queryable
+{
+
+    public class ConcatenatedQueryable<TElement> : IQueryable<TElement>, IQueryProvider, IOrderedQueryable<TElement>
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ConcatenatedQueryable(IQueryable<TElement> firstQuery, IQueryable<TElement> secondQuery)
+            => this.Expression = new ConcatenationExpression(firstQuery, secondQuery);
+
+        private ConcatenatedQueryable(Expression expression)
+            => this.Expression = expression;
+
+        #endregion Constructors
+
+        #region - - - - - - IQueryable Implementation - - - - - -
+
+        public Type ElementType
+            => typeof(TElement);
+
+        public Expression Expression { get; }
+
+        public IQueryProvider Provider
+            => this;
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return null;
+
+            //var _X = new ConcatenatedQueryableExpressionVisitor().Visit(this.Expression);
+            //var _Y = new ReduceExpressionVisitor().Visit(_X);
+
+            //return Expression
+            //    .Lambda<Func<IQueryable<TElement>>>(
+            //        Expression.Convert(
+            //            _Y,
+            //            typeof(IQueryable<>).MakeGenericType(this.ElementType)))
+            //    .Compile()
+            //    .Invoke()
+            //    .GetEnumerator();
+        }
+        //=> Expression
+        //    .Lambda<Func<IQueryable<TElement>>>(
+        //        Expression.Convert(
+        //            new ConcatenatedQueryableExpressionVisitor().Visit(this.Expression).Reduce(),
+        //            typeof(IQueryable<>).MakeGenericType(this.ElementType)))
+        //    .Compile()
+        //    .Invoke()
+        //    .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => ((IQueryable<TElement>)this).GetEnumerator();
+
+        #endregion IQueryable Implementation
+
+        #region - - - - - - IQueryProvider Implementation - - - - - -
+
+        public IQueryable CreateQuery(Expression expression)
+            => (IQueryable)Activator.CreateInstance(typeof(ConcatenatedQueryable<>).MakeGenericType(expression.Type.GenericTypeArguments[0]), new XVisitor().Visit(expression));
+
+        public IQueryable<T> CreateQuery<T>(Expression expression)
+            => new ConcatenatedQueryable<T>(new XVisitor().Visit(expression));
+
+        public object Execute(Expression expression)
+            => throw new NotImplementedException();
+
+        public TResult Execute<TResult>(Expression expression)
+            => throw new NotImplementedException();
+
+        #endregion IQueryProvider Implementation
+
+    }
+
+
+    public class XVisitor : ExpressionVisitor
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            var _Arguments = this.Visit(node.Arguments);
+
+            xxx // Visiting a node with children will reduce them...
+
+            var _Concatenation = _Arguments.OfType<ConcatenationExpression>().SingleOrDefault();
+            if (_Concatenation == null)
+                return base.VisitMethodCall(node);
+
+            var _Object = this.Visit(node.Object);
+            var _ConcatenationIndex = _Arguments.IndexOf(_Concatenation);
+
+            var _FirstArgumentArray = _Arguments.ToArray();
+            _FirstArgumentArray[_ConcatenationIndex] = _Concatenation.First;
+
+            var _SecondArgumentArray = _Arguments.ToArray();
+            _SecondArgumentArray[_ConcatenationIndex] = _Concatenation.Second;
+
+            return new ConcatenationExpression(
+                Expression.Call(_Object, node.Method, _FirstArgumentArray),
+                Expression.Call(_Object, node.Method, _SecondArgumentArray));
+        }
+
+        #endregion Methods
+
+    }
+
+
+    //public class ConcatenatedQueryableExpressionVisitor : ExpressionVisitor
+    //{
+
+    //    #region - - - - - - Methods - - - - - -
+
+    //    [return: NotNullIfNotNull("node")]
+    //    public override Expression Visit(Expression node)
+    //        => node is ConcatenationExpression _Node
+    //            ? new DuplicateTreeExpression(this.Visit(_Node.FirstQuery.Expression), this.Visit(_Node.SecondQuery.Expression))
+    //            : base.Visit(node);
+
+    //    protected override Expression VisitMethodCall(MethodCallExpression node)
+    //    {
+    //        var _Arguments = this.Visit(node.Arguments);
+
+    //        var _DuplicateTree = _Arguments.OfType<DuplicateTreeExpression>().SingleOrDefault();
+    //        if (_DuplicateTree == null)
+    //            return base.VisitMethodCall(node);
+
+    //        var _Object = this.Visit(node.Object);
+    //        var _DuplicateTreeIndex = _Arguments.IndexOf(_DuplicateTree);
+
+    //        var _FirstArgumentArray = _Arguments.ToArray();
+    //        _FirstArgumentArray[_DuplicateTreeIndex] = _DuplicateTree.First;
+
+    //        var _SecondArgumentArray = _Arguments.ToArray();
+    //        _SecondArgumentArray[_DuplicateTreeIndex] = _DuplicateTree.Second;
+
+    //        return new DuplicateTreeExpression(
+    //            Expression.Call(_Object, node.Method, _FirstArgumentArray),
+    //            Expression.Call(_Object, node.Method, _SecondArgumentArray));
+    //    }
+
+    //    #endregion Methods
+
+    //}
+
+    internal class ConcatenationExpression : Expression
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private static MethodInfo s_ConcatMethodInfo;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ConcatenationExpression(Expression first, Expression second)
+        {
+            this.First = first ?? throw new ArgumentNullException(nameof(first));
+            this.Second = second ?? throw new ArgumentNullException(nameof(second));
+        }
+
+        public ConcatenationExpression(IQueryable firstQuery, IQueryable secondQuery) : this(
+            Constant(firstQuery ?? throw new ArgumentNullException(nameof(firstQuery))),
+            Constant(secondQuery ?? throw new ArgumentNullException(nameof(secondQuery))))
+        { }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public override bool CanReduce
+            => true;
+
+        private Type ElementType
+            => this.First.Type.GenericTypeArguments[0];
+
+        public Expression First { get; }
+
+        public override ExpressionType NodeType
+            => (ExpressionType)int.MinValue;
+
+        public Expression Second { get; }
+
+        public override Type Type
+            => typeof(IQueryable<>).MakeGenericType(this.ElementType);
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        private static IQueryable<TElement> Concat<TElement, TFirstQuery, TSecondQuery>(TFirstQuery first, TSecondQuery second)
+            where TFirstQuery : IQueryable<TElement>
+            where TSecondQuery : IQueryable<TElement>
+            => first.Concat(second);
+
+        private static MethodInfo GetConcatMethodInfo(Type TElement, Type TFirstQuery, Type TSecondQuery)
+            => (s_ConcatMethodInfo ??= typeof(ConcatenationExpression).GetMethod(nameof(Concat), BindingFlags.NonPublic | BindingFlags.Static).GetGenericMethodDefinition())
+                .MakeGenericMethod(TElement, TFirstQuery, TSecondQuery);
+
+        public override Expression Reduce()
+            => Call(null,
+                GetConcatMethodInfo(this.ElementType, this.First.Type, this.Second.Type),
+                this.First,
+                 this.Second);
+
+        #endregion Methods
+
+    }
+
+    //internal class DuplicateTreeExpression : Expression
+    //{
+
+    //    #region - - - - - - Fields - - - - - -
+
+    //    private static MethodInfo s_ConcatMethodInfo;
+
+    //    #endregion Fields
+
+    //    #region - - - - - - Constructors - - - - - -
+
+    //    public DuplicateTreeExpression(Expression first, Expression second)
+    //    {
+    //        this.First = first ?? throw new ArgumentNullException(nameof(first));
+    //        this.Second = second ?? throw new ArgumentNullException(nameof(second));
+    //    }
+
+    //    #endregion Constructors
+
+    //    #region - - - - - - Properties - - - - - -
+
+    //    public override bool CanReduce
+    //        => true;
+
+    //    private Type ElementType
+    //        => this.First.Type.GenericTypeArguments[0];
+
+    //    public Expression First { get; }
+
+    //    public override ExpressionType NodeType
+    //        => (ExpressionType)int.MinValue;
+
+    //    public Expression Second { get; }
+
+    //    public override Type Type
+    //        => typeof(IQueryable<>).MakeGenericType(this.ElementType);
+
+    //    #endregion Properties
+
+    //    #region - - - - - - Methods - - - - - -
+
+    //    private static IQueryable<TElement> Concat<TElement, TFirstQuery, TSecondQuery>(TFirstQuery first, TSecondQuery second)
+    //        where TFirstQuery : IQueryable<TElement>
+    //        where TSecondQuery : IQueryable<TElement>
+    //        => first.Concat(second);
+
+    //    private static MethodInfo GetConcatMethodInfo(Type TElement, Type TFirstQuery, Type TSecondQuery)
+    //        => (s_ConcatMethodInfo ??= typeof(DuplicateTreeExpression).GetMethod(nameof(Concat), BindingFlags.NonPublic | BindingFlags.Static).GetGenericMethodDefinition())
+    //            .MakeGenericMethod(TElement, TFirstQuery, TSecondQuery);
+
+    //    public override Expression Reduce()
+    //        => Call(null,
+    //            GetConcatMethodInfo(this.ElementType, this.First.Type, this.Second.Type),
+    //            this.First,
+    //             this.Second);
+
+    //    #endregion Methods
+
+    //}
+
+    internal class ReduceExpressionVisitor : ExpressionVisitor
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        [return: NotNullIfNotNull("node")]
+        public override Expression Visit(Expression node)
+            => (node?.CanReduce ?? false) ? this.Visit(node.ReduceAndCheck()) : base.Visit(node);
+
+        #endregion Methods
+
+    }
+
+}
